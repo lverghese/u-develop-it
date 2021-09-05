6f97cda8e9d64e33381d465fe4cd0cc5fafa0baa
lverghese <loganverghese@gmail.com>
2021-09-05 17:33:40 -0400

    server.js init

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+//Default response for any other request (not found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+//listening to port url
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
