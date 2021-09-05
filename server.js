87ff22b4dc1fb2d89f8865c69883ccbeefe09954
lverghese <loganverghese@gmail.com>
2021-09-05 17:54:15 -0400

    db queries for create and delete

--- server.js
+++ server.js
@@ -21,13 +21,40 @@ const db = mysql.createConnection(
     console.log('Connected to the election database')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log (err);
+//     }
+//     console.log(row);
+// });
 
 //Default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
+});
+
+// //delete a cadidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 //listening to port url
